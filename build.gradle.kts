b1d6f530ac1b3f8e3876bc5f6bd7052bdf9d3742
Yusuf Nasser <YusufNasserEng@gmail.com>
2025-04-28 01:05:58 +0300

    add github action for jacoco test coverage enforcement (#1)
    * add testing & jacoco dependencies
    
    * add github test coverage action
    
    * add test function

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("jacoco")
 }
 
 group = "org.damascus"
@@ -10,12 +11,71 @@ repositories {
 }
 
 dependencies {
+    implementation("io.insert-koin:koin-core:4.0.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation(kotlin("test"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.2")
+
+    testImplementation("com.google.truth:truth:1.4.4")
+    testImplementation("io.mockk:mockk:1.14.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
+    }
+    finalizedBy(tasks.jacocoTestReport)
 }
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+    dependsOn(tasks.test)
+}
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            classDirectories.setFrom(
+                files(classDirectories.files.map { file ->
+                    fileTree(file) {
+                        exclude("**/model/**")
+                        exclude("**/di/**")
+                    }
+                })
+            )
+
+            limit {
+                minimum = "0.8".toBigDecimal()
+            }
+
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.8".toBigDecimal()
+            }
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.8".toBigDecimal()
+            }
+            limit {
+                counter = "METHOD"
+                value = "COVEREDRATIO"
+                minimum = "0.8".toBigDecimal()
+            }
+        }
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.13"
+}
+
 kotlin {
     jvmToolchain(17)
 }
