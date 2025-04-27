4ae6b262d8431f1bdb8586912336c402eedf43f2
Yusuf Nasser <yusufnassereng@gmail.com>
2025-04-27 21:41:46 +0300

    add testing & jacoco dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("jacoco")
 }
 
 group = "org.damascus"
@@ -10,12 +11,68 @@ repositories {
 }
 
 dependencies {
+    implementation("io.insert-koin:koin-core:4.0.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation(kotlin("test"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.2")
+
+    testImplementation("com.google.truth:truth:1.4.4")
+    testImplementation("io.mockk:mockk:1.14.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+    dependsOn(tasks.test)
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        classDirectories.setFrom(
+            classDirectories.files.forEach {
+                fileTree(it) {
+                    exclude("**/model/**")
+                    exclude("**/di/**")
+                }
+            }
+        )
+        rule {
+            limit {
+                minimum = "0.8".toBigDecimal()
+            }
+
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.8".toBigDecimal()
+            }
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.8".toBigDecimal()
+            }
+            limit {
+                counter = "METHOD"
+                value = "COVEREDRATIO"
+                minimum = "0.8".toBigDecimal()
+            }
+        }
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.13"
+}
+
 kotlin {
     jvmToolchain(17)
 }
