1736e27490c40adf0b95810789332b4602ed3242
Moudyash <medo.ash.2001@gmail.com>
2025-05-03 07:06:01 +0300

    Update CreateMateUseCase imports

--- src/main/kotlin/logic/useCase/CreateMateUseCase.kt
+++ src/main/kotlin/logic/useCase/CreateMateUseCase.kt
@@ -3,8 +3,6 @@ package logic.useCase.mate
 import logic.model.Admin
 import logic.model.Mate
 import logic.model.User
-import org.damascus.data.authentication.MD5HashingService
-import org.damascus.logic.AuthenticationRepository
 import org.damascus.logic.exception.*
 
 class CreateMateUseCase(
