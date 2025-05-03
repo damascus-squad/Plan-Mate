ad13073f3e5104c7ea57b9e5f112cf09c0b486ec
Mohamed Boumaraf <boumarafmohamed.co@gmail.com>
2025-05-02 22:47:12 -0700

    improve ProjectRepositoryImpl

--- src/main/kotlin/data/repo/ProjectRepositoryImpl.kt
+++ src/main/kotlin/data/repo/ProjectRepositoryImpl.kt
@@ -1,83 +1,46 @@
 package org.damascus.data.repo
 
 
-
 import logic.model.Project
 import org.damascus.data.DataSource
 import org.damascus.logic.repository.ProjectRepository
 import java.util.*
 
 
-class ProjectNotFoundException(projectId: UUID) :
-    RuntimeException("Project with ID $projectId not found")
-
-class ProjectRepositoryImpl(private val projectDataSource: DataSource<Project>) : ProjectRepository {
+class ProjectRepositoryImpl(private val dataSource: DataSource<Project>) : ProjectRepository {
 
     override fun create(project: Project): Boolean {
-        val existingProjects = projectDataSource.read()
-        if (existingProjects.any { it.id == project.id }) return false
-        projectDataSource.write(project)
+        if (dataSource.read().any { it.id == project.id }) return false
+        dataSource.write(project)
         return true
     }
 
     override fun update(projectId: UUID, project: Project): Boolean {
-        val existingProjects = projectDataSource.read()
-        if (existingProjects.none { it.id == projectId }) return false
-        projectDataSource.update(projectId, project)
+        if (dataSource.read().none { it.id == projectId }) return false
+        dataSource.update(projectId, project)
         return true
     }
 
     override fun delete(projectId: UUID): Boolean {
-        val existingProjects = projectDataSource.read()
-        if (existingProjects.none { it.id == projectId }) return false
-        projectDataSource.delete(projectId)
+        if (dataSource.read().none { it.id == projectId }) return false
+        dataSource.delete(projectId)
         return true
     }
 
     override fun exists(projectId: UUID): Boolean {
-        return projectDataSource.read().any { it.id == projectId }
+        return dataSource.read().any { it.id == projectId }
     }
 
     override fun get(projectId: UUID): Project {
-        return projectDataSource.read().firstOrNull { it.id == projectId }
+        return dataSource.read().firstOrNull { it.id == projectId }
             ?: throw ProjectNotFoundException(projectId)
     }
 
     override fun getAll(): List<Project> {
-        return projectDataSource.read()
+        return dataSource.read()
     }
 
-    override fun assignMate(projectId: UUID, mateId: UUID): Boolean {
-        val existingProjects = projectDataSource.read()
-
-        val project = existingProjects.firstOrNull { it.id == projectId }
-            ?: return false
-
-        if (project.assignedMatesIds.contains(mateId))
-            return false
-
-        val updatedProject = project.copy(
-            assignedMatesIds = project.assignedMatesIds.toMutableList().apply { add(mateId) }
-        )
-
-        projectDataSource.update(project.id, updatedProject)
-        return true
-    }
-
-    override fun unassignMate(projectId: UUID, mateId: UUID): Boolean {
-        val existingProjects = projectDataSource.read()
-
-        val project = existingProjects.firstOrNull { it.id == projectId }
-            ?: return false
-
-        if (!project.assignedMatesIds.contains(mateId))
-            return false
-
-        val updatedProject = project.copy(
-            assignedMatesIds = project.assignedMatesIds.toMutableList().apply { remove(mateId) }
-        )
-
-        projectDataSource.update(project.id, updatedProject)
-        return true
+    override fun getAllProjectsByMateId(mateId: UUID): List<Project> {
+        return dataSource.read().filter { mateId in it.assignedMatesIds }
     }
 }
