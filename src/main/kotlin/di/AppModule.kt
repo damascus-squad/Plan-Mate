d69a5c54a1c26be5edf67532754f3e04bf76379f
Asmaa Karam <asmaakaram793@gmai.com>
2025-05-03 15:58:28 +0300

    add task view cli implementation

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,6 +1,9 @@
 package org.damascus.di
 
+import data.csv.helpers.TaskCsvHelper
 import data.csv.helpers.UserCsvHelper
+import data.repo.TaskRepositoryImpl
+import logic.model.Task
 import logic.model.User
 import org.damascus.data.authentication.AuthenticationRepoImpl
 import org.damascus.data.authentication.MD5HashingService
@@ -9,11 +12,17 @@ import org.damascus.data.csv.generateCsvHeader
 import org.damascus.data.csv.utils.CsvConstants.USERS_FILE
 import org.damascus.logic.repository.AuthenticationRepository
 import logic.repo.DataSource
+import logic.repo.TaskRepository
+import logic.repo.TaskStateRepository
+import org.damascus.data.csv.utils.CsvConstants.TASKS_FILE
+import org.damascus.data.repo.TaskStateRepositoryImpl
 import org.damascus.logic.service.HashingService
 import org.damascus.logic.usecase.AuthenticationUseCase
+import org.damascus.logic.usecase.task.*
 import org.damascus.ui.io.ConsoleDisplay
 import org.damascus.ui.io.ConsoleUserInput
 import org.damascus.ui.PlanMateConsoleUi
+import org.damascus.ui.views.task.TaskCLI
 import org.koin.dsl.module
 
 val appModule = module {
@@ -28,12 +37,30 @@ val appModule = module {
         )
     }
 
+    single<DataSource<Task>> {
+        CsvDataSource(
+            TASKS_FILE,
+            { generateCsvHeader<Task>() },
+            extractId = { it.id },
+            parser = TaskCsvHelper::parseTask,
+            serializer = TaskCsvHelper::serializeTask
+        )
+    }
+
     single<AuthenticationRepository> { AuthenticationRepoImpl(get(), get()) }
+    single<TaskStateRepository> { TaskStateRepositoryImpl(get()) }
+    single<TaskRepository> { TaskRepositoryImpl(get()) }
     single<HashingService> { MD5HashingService() }
     single { AuthenticationUseCase(get()) }
+    single { UpdateTaskUseCase(get()) }
+    single { CreateTaskUseCase(get()) }
+    single { DeleteTaskUseCase(get()) }
+    single { GetTaskUseCase(get()) }
+    single { GetTasksByProjectUseCase(get()) }
 
     // UI
     single { ConsoleUserInput() }
     single { ConsoleDisplay(get()) }
     single { PlanMateConsoleUi(get()) }
+    single { TaskCLI(get(),get(),get(),get(),get(),get() )}
 }
---
+++ src/main/kotlin/logic/usecase/task/GetTasksByProjectUseCase.kt
@@ -0,0 +1,9 @@
+package org.damascus.logic.usecase.task
+
+import logic.model.Task
+import logic.repo.TaskRepository
+import java.util.*
+
+class GetTasksByProjectUseCase (private val repository: TaskRepository) {
+    operator fun invoke(projectId: UUID): List<Task> = repository.getByProject(projectId)
+}
