4842e0d0dfdb19dffb9e5bfa404e5e919aecc9a4
Yousif Murad <yousifmahmud420@gmail.com>
2025-05-04 22:32:52 +0300

    bind AuditLogRepository with AuditLogRepositoryImpl and complete its saveLog method with delete blank line in AppModule.kt

--- src/main/kotlin/data/repo/AuditLogRepositoryImpl.kt
+++ src/main/kotlin/data/repo/AuditLogRepositoryImpl.kt
@@ -7,13 +7,14 @@ import java.util.*
 
 class AuditLogRepositoryImpl(private val dataSource: DataSource<History>): AuditLogRepository{
     override fun saveLog(history: History){
+        dataSource.write(history) // or .save(history) depending on the function name
     }
 
     override fun getLogByProjectId(projectId: UUID): List<History>{
-        return dataSource.read().filter {it.id == projectId}
+        return dataSource.read().filter {it.projectId == projectId}
     }
 
     override fun getLogByTaskId(taskId: UUID): List<History>{
-        return dataSource.read().filter {it.id == taskId}
+        return dataSource.read().filter {it.taskId == taskId}
     }
 }
