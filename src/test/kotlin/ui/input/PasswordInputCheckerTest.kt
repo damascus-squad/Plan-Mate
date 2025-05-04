473bcac19b8fb81b2f973ec1de143d732526c5ef
Harith Bashar <harithbashar@gmail.com>
2025-05-05 00:30:23 +0300

    add mate creation view feature (#26)
    * add mate creation view feature
    
    * change functions names and add the names in test and add some private const variables
    
    * resolve merge conflicts with develop
    
    ---------
    
    Co-authored-by: Yusuf Nasser <yusufnassereng@gmail.com>

---
+++ src/test/kotlin/ui/input/PasswordInputCheckerTest.kt
@@ -0,0 +1,80 @@
+package ui.input
+
+import org.damascus.ui.input.checkPasswordInput
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+class PasswordInputCheckerTest {
+
+    @Test
+    fun `checkPasswordInput should return false when first letter is not digit or letter`() {
+        // Given
+        val input = "_harith"
+
+        // When
+        val result = checkPasswordInput(input)
+
+        // Then
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `checkPasswordInput should return false when password is empty`() {
+        // Given
+        val input = ""
+
+        // When
+        val result = checkPasswordInput(input)
+
+        // Then
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `checkPasswordInput should return false when password is filed with spaces`() {
+        // Given
+        val input = "      "
+
+        // When
+        val result = checkPasswordInput(input)
+
+        // Then
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `checkPasswordInput should return false when password ends with space`() {
+        // Given
+        val input = "H@rIth "
+
+        // When
+        val result = checkPasswordInput(input)
+
+        // Then
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `checkPasswordInput should return false when password length is less than 6 characters`() {
+        // Given
+        val input = "H@ri"
+
+        // When
+        val result = checkPasswordInput(input)
+
+        // Then
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `checkPasswordInput should return false when password length is more than 20 characters`() {
+        // Given
+        val input = "harithAbdulrahmanYosufAhmed"
+
+        // When
+        val result = checkPasswordInput(input)
+
+        // Then
+        assertEquals(false, result)
+    }
+}
