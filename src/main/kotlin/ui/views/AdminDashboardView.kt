c3705d9a5f9ff183f99257cfc70bd69a8182889e
Mohamed Boumaraf <boumarafmohamed.co@gmail.com>
2025-05-06 09:57:34 -0700

    refactor AdminDashboardView

--- src/main/kotlin/ui/views/AdminDashboardView.kt
+++ src/main/kotlin/ui/views/AdminDashboardView.kt
@@ -2,18 +2,15 @@ package org.damascus.ui.views
 
 import logic.model.Admin
 import org.damascus.logic.model.Role
-import org.damascus.ui.io.InputReader
 import org.damascus.ui.views.project.ProjectView
 import org.damascus.ui.util.TerminalColor
 import org.damascus.ui.util.withStyle
 import logic.usecase.auth.CreateMateUseCase
 import org.damascus.ui.io.ConsoleDisplay
-import org.damascus.ui.io.ConsoleUserInput
 import org.damascus.ui.util.UiAction
 
-
 class AdminDashboardView(
-    private val inputReader: InputReader,
+    private val consoleDisplay: ConsoleDisplay,
     private val projectView: ProjectView,
     private val createMateUseCase: CreateMateUseCase
 ) {
@@ -22,14 +19,10 @@ class AdminDashboardView(
             println("Only admins can access this dashboard!".withStyle(TerminalColor.Red))
             return
         }
-
-        val consoleDisplay = ConsoleDisplay(inputReader as ConsoleUserInput)
-
         val dashboardActions = listOf(
             UiAction("See ALL Projects") { projectView.showAllProjects() },
             UiAction("Create New Mate") { viewMateCreation(admin, createMateUseCase) }
         )
-
         consoleDisplay.displayMenu(dashboardActions, "ADMIN DASHBOARD")
     }
 }
