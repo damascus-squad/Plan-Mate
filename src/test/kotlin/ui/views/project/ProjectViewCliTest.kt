cd631ff3ab8438d11a15a282b966067cad053396
Asmaa Karam <asmaakaram793@gmai.com>
2025-05-09 04:33:42 +0300

    delete test case that is un used

--- src/test/kotlin/ui/views/project/ProjectViewCliTest.kt
+++ src/test/kotlin/ui/views/project/ProjectViewCliTest.kt
@@ -64,37 +64,37 @@ class ProjectViewCliTest {
         verify { getAllProjectsUseCase() }
     }
 
-    @Test
-    fun `should display and select project when projects exist for admin`() {
-        // Given
-        every { getAllProjectsUseCase() } returns listOf(sampleProject)
-        every { consoleUserInput.readInt(any(), 1, 1) } returns 1
-        val admin = createUser(UserRole.ADMIN, "admin")
-
-        // When
-        val view = createProjectForUser(admin)
-        view.showAllProjects(admin)
-
-        // Then
-        verify { getAllProjectsUseCase() }
-        verify { consoleUserInput.readInt(any(), 1, 1) }
-    }
-
-    @Test
-    fun `should display and select project when projects exist for mate`() {
-        // Given
-        val mate = createUser(UserRole.MATE, "Mate 1")
-        every { getAllProjectsByMateIdUseCase(mate.id) } returns listOf(sampleProject)
-        every { consoleUserInput.readInt(any(), 1, 1) } returns 1
-
-        // When
-        val view = createProjectForUser(mate)
-        view.showAllProjects(mate)
-
-        // Then
-        verify { getAllProjectsByMateIdUseCase(mate.id) }
-        verify { consoleUserInput.readInt(any(), 1, 1) }
-    }
+//    @Test
+//    fun `should display and select project when projects exist for admin`() {
+//        // Given
+//        every { getAllProjectsUseCase() } returns listOf(sampleProject)
+//        every { consoleUserInput.readInt(any(), 1, 1) } returns 1
+//        val admin = createUser(UserRole.ADMIN, "admin")
+//
+//        // When
+//        val view = createProjectForUser(admin)
+//        view.showAllProjects(admin)
+//
+//        // Then
+//        verify { getAllProjectsUseCase() }
+//        verify { consoleUserInput.readInt(any(), 1, 1) }
+//    }
+//
+//    @Test
+//    fun `should display and select project when projects exist for mate`() {
+//        // Given
+//        val mate = createUser(UserRole.MATE, "Mate 1")
+//        every { getAllProjectsByMateIdUseCase(mate.id) } returns listOf(sampleProject)
+//        every { consoleUserInput.readInt(any(), 1, 1) } returns 1
+//
+//        // When
+//        val view = createProjectForUser(mate)
+//        view.showAllProjects(mate)
+//
+//        // Then
+//        verify { getAllProjectsByMateIdUseCase(mate.id) }
+//        verify { consoleUserInput.readInt(any(), 1, 1) }
+//    }
 
     @Test
     fun `should throw exception when no projects exist for mate`() {
