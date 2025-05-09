eaafc6b93e6b581058add86b52bb9b4b01940e94
Asmaa Karam <asmaakaram793@gmai.com>
2025-05-09 05:50:58 +0300

    add CreateProjectUi class for admin project creation

---
+++ src/main/kotlin/ui/views/project/CreateProjectUi.kt
@@ -0,0 +1,33 @@
+package org.damascus.ui.views.project
+
+import logic.model.Project
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import logic.usecase.project.CreateProjectUseCase
+import ui.io.Display
+import ui.io.InputReader
+import java.util.*
+
+class CreateProjectUi (
+    private val inputReader:InputReader,
+    private val display: Display,
+    private val createProjectUseCase: CreateProjectUseCase
+){
+    operator fun invoke(){
+        val name = inputReader.readString("Enter project name:")
+        val project = Project(
+            id = UUID.randomUUID(),
+            name = name,
+            assignedMatesIds = mutableListOf(),
+            allowedStatesIds = mutableListOf(),
+            creationDate = Clock.System.now().toLocalDateTime(TimeZone.UTC)
+        )
+
+        if (createProjectUseCase(project)) {
+            display.write("Added Project ${project.name}")
+        } else {
+            display.writeError("Project already exists.")
+        }
+    }
+}
