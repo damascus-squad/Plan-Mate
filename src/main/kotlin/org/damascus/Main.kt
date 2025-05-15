943c47b30e89600a9357a42b7e9c92447affb9ee
Yusuf Nasser <yusufnassereng@gmail.com>
2025-05-15 04:02:39 +0300

    add @KoverIgnore to ignore main function

--- src/main/kotlin/org/damascus/Main.kt
+++ src/main/kotlin/org/damascus/Main.kt
@@ -1,5 +1,6 @@
 package org.damascus
 
+import org.damascus.annotation.KoverIgnore
 import org.damascus.di.appModule
 import org.damascus.di.repositoryModule
 import org.damascus.di.useCaseModule
@@ -7,6 +8,7 @@ import org.damascus.ui.PlanMateConsoleUi
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
 
+@KoverIgnore("Main function")
 fun main() {
     startKoin {
         modules(appModule, repositoryModule, useCaseModule)
