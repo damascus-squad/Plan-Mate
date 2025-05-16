caa0e9d9522f7f35e4bbb2010320f9182ef1e304
yousif-murad <93213585+YousifMurad@users.noreply.github.com>
2025-05-16 15:53:21 +0300

    refactor by extract headers and usageRow variable into functions

--- src/main/kotlin/org/damascus/ui/views/taskState/GetAllTaskStateUi.kt
+++ src/main/kotlin/org/damascus/ui/views/taskState/GetAllTaskStateUi.kt
@@ -1,28 +1,35 @@
 package org.damascus.ui.views.taskState
 
+import org.damascus.logic.model.TaskState
 import org.damascus.logic.repo.TaskStateRepository
 import org.damascus.ui.util.TerminalColor
 import org.damascus.ui.util.printTable
-import org.damascus.ui.util.withStyle
 import org.koin.core.annotation.Single
 
 @Single
-class GetAllTaskStateUi(
-    private val taskStateRepository: TaskStateRepository,
-){
+class GetAllTaskStateUi(private val repository: TaskStateRepository) {
     operator fun invoke() {
-    val states = taskStateRepository.getAllStates()
-    if (states.isEmpty()) {
-        println("❗ No states found.".withStyle(TerminalColor.Red))
-        return
+        val states = repository.getAllStates()
+        if (states.isEmpty()) {
+            println("⚠️ No task states found.")
+            return
+        }
+        val headers = buildHeaders(states)
+        val usageRow = buildUsageRow(states)
+
+        printTable(headers, usageRow)
     }
 
-    val headers = listOf("State Name") + states.map { it.name }
-    val usageRow = listOf("Used In") + states.map {
-        val count = it.projectReferencesCount
-        "$count project" + if (count != 1) "s" else ""
-    }
+    fun buildHeaders(states: List<TaskState>): List<String> =
+        listOf("State Name") + states.map { it.name }
 
-    printTable(headers, listOf(usageRow), TerminalColor.Green)
-}
+    fun buildUsageRow(states: List<TaskState>): List<String> =
+        listOf("Used In") + states.map {
+            val count = it.projectReferencesCount
+            "$count project" + if (count != 1) "s" else ""
+        }
+
+    private fun printTable(headers: List<String>, usageRow: List<String>) {
+        printTable(headers, listOf(usageRow), TerminalColor.Green)
+    }
 }
