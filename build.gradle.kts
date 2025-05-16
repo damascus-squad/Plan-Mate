bb0e38360d97bc2b4e152c1bff62c5b833d47ebe
Yusuf Nasser <yusufnassereng@gmail.com>
2025-05-16 10:48:36 +0300

    add testcontainers dependencies to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+    testImplementation("org.testcontainers:junit-jupiter:1.20.6")
+    testImplementation("org.testcontainers:mongodb:1.18.3")
 }
 
 tasks.test {
