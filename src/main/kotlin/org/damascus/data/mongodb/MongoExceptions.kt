c38cb38eaeec30da54f483e73d7fc12bc355bdcc
Yusuf Nasser <yusufnassereng@gmail.com>
2025-05-17 18:06:37 +0300

    add MongoDocumentNotFound exception class

--- src/main/kotlin/org/damascus/data/mongodb/MongoExceptions.kt
+++ src/main/kotlin/org/damascus/data/mongodb/MongoExceptions.kt
@@ -1,8 +1,5 @@
 package org.damascus.data.mongodb
 
-import com.mongodb.MongoException
-import jdk.internal.joptsimple.internal.Messages.message
-
 class CredentialsNotFound(message: String) : Exception(message)
 
-class CollectionIsEmptyException() : MongoException("Mongo Collection is Empty")
+class MongoDocumentNotFound : Exception()
