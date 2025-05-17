c76f0e2aff6fb5a90a11c832a8224867f350d14e
Yusuf Nasser <yusufnassereng@gmail.com>
2025-05-17 18:06:58 +0300

    update MongoDataSource implementation

--- src/main/kotlin/org/damascus/data/mongodb/MongoDataSource.kt
+++ src/main/kotlin/org/damascus/data/mongodb/MongoDataSource.kt
@@ -1,8 +1,6 @@
 package org.damascus.data.mongodb
 
-import com.mongodb.MongoBulkWriteException
 import com.mongodb.MongoException
-import com.mongodb.MongoWriteException
 import com.mongodb.client.model.Filters
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import kotlinx.coroutines.CoroutineDispatcher
@@ -22,57 +20,46 @@ class MongoDataSource<T : MongoDocument>(
 
     private val collection = mongoDatabase.getCollection(collectionName, documentClass)
 
-    suspend fun read(): List<T> {
-        val result = withContext(dispatcher) {
-            try {
-                collection.find().toList()
-            } catch (e: MongoException) {
-                throw e
-            }
-        }
-
-        if (result.isEmpty())
-            throw CollectionIsEmptyException()
-
-        return result
+    suspend fun read(): List<T> = withContext(dispatcher) {
+        collection.find().toList()
     }
 
     suspend fun write(entry: T) = withContext(dispatcher) {
         try {
             collection.insertOne(entry)
-        } catch (e: MongoWriteException) {
+        } catch (e: MongoException) {
             throw e
         }
     }
 
-
     suspend fun write(entriesList: List<T>) = withContext(dispatcher) {
         try {
             collection.insertMany(entriesList)
-        } catch (e: MongoBulkWriteException) {
+        }
+        catch (e: MongoException) {
             throw e
         }
     }
 
     suspend fun update(id: UUID, updatedData: T) = withContext(dispatcher) {
-        try {
-            collection.findOneAndUpdate(
-                Filters.eq(MONGO_ID, id),
-                Document(
-                    MONGO_SET_OPERATOR,
-                    updatedData.toUpdateDocument()
-                )
+        collection.findOneAndUpdate(
+            Filters.eq(MONGO_ID, id),
+            Document(
+                MONGO_SET_OPERATOR,
+                updatedData.toUpdateDocument()
             )
-        } catch (e: MongoWriteException) {
-            throw e
+        ).apply {
+            if (this == null) {
+                throw MongoDocumentNotFound()
+            }
         }
     }
 
     suspend fun delete(id: UUID) = withContext(dispatcher) {
-        try {
-            collection.findOneAndDelete(Filters.eq(MONGO_ID, id))
-        } catch (e: MongoWriteException) {
-            throw e
+        collection.findOneAndDelete(Filters.eq(MONGO_ID, id)).apply {
+            if (this == null) {
+                throw MongoDocumentNotFound()
+            }
         }
     }
 }
